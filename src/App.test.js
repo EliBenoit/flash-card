31628e1dc40b7b291d1b64e6e584bbf1d000794b
Eli <eli.vercollier@gmail.com>
2025-11-14 15:39:44 +0100

    Create project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+/* Commented test since it's unrelevent for now
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
