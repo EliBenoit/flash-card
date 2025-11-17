778396a7162f54ce0f3ced2a11fdc518859d6f11
Eli <eli.vercollier@gmail.com>
2025-11-17 17:01:51 +0100

    chore: Add a global export for ui-component

---
+++ src/ui-component/index.js
@@ -0,0 +1,13 @@
+import Button from './Button/Button';
+import Card from './Card/Card';
+import CardHeader from './Card/CardHeader/CardHeader';
+import CategoryButton from './CategoryButton/CategoryButton';
+import ProgressBar from './ProgressBar/ProgressBar';
+
+export {
+    Button,
+    Card,
+    CardHeader,
+    CategoryButton,
+    ProgressBar
+}
